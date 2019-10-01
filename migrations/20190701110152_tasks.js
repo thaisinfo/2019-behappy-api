4561c10e8d183f92c060c0dcad42133d45b11e71
thaisinfo <47748343+thaisinfo@users.noreply.github.com>
2019-09-30 23:40:25 -0300

    Update 20190701110152_tasks.js

--- migrations/20190701110152_tasks.js
+++ migrations/20190701110152_tasks.js
@@ -2,11 +2,11 @@
 const table_name = "tasks";
 
 exports.up = function(knex, Promise) {
-  return knex.schema.createTable(table_name, table => {
-    table.increments("oid").primary();
-    table.string("title").notNullable();
-    table.string("description").nullable();
-    table.timestamps(false, true);
+  return knex.schema.createTable(table_name, (table) => {
+    table.increments('oid').primary();
+    table.string('title').notNullable();
+    table.string('description').nullable();
+    table.timestamps();
   });
 };
 
